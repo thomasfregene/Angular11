189d47db4e06f0b9bc104b3507fa78d1ca480d82
thomasfregene <fregzythomas@gmail.com>
2021-04-22 05:28:54 +0100

    providing routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DepartmentComponent } from './department/department.component';
+import { EmployeeComponent } from './employee/employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'department', component:DepartmentComponent},
+  {path:'employee', component:EmployeeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
