7740f442ea24d22cc5d744e61821c736e07c9236
thomasfregene <fregzythomas@gmail.com>
2021-04-24 10:42:56 +0100

    @Input directive to pass prop

--- src/app/department/add-edit-dept/add-edit-dept.component.ts
+++ src/app/department/add-edit-dept/add-edit-dept.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-add-edit-dept',
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class AddEditDeptComponent implements OnInit {
 
   constructor() { }
+
+  @Input
 
   ngOnInit(): void {
   }
